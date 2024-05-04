c8c80d4786e8cb8ad2c10a4940112f448257f4e2
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-05-04 22:02:48 +0100

    Adding CSS & Images

---
+++ Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import lodash from 'lodash';
+import "../css/main.css"
+
+const $button = $('<button>Click here to get started</button>');
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append($button);
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+const updateCounter = () => {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+};
+
+$button.on('click', lodash.debounce(updateCounter, 500));
---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./js/dashboard_main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public/"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
