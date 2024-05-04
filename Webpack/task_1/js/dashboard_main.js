b0bba954eda19e7ddcc0bc73a351ed6a19b067f7
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-05-04 13:29:01 +0100

    Learning how to use Webpack with a config file

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,17 +1,16 @@
-import $ from 'jquery';
-import lodash from 'lodash';
+import $ from "jquery";
+import lodash from "lodash";
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button id="btn">Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append('<button id="btn">Click here to get started</button>');
+$("body").append('<p id="count"></p>');
+$("body").append("<p>Copyright - Holberton School</p>");
 
 let count = 0;
 const updateCounter = () => {
   count++;
-  $('#count').text(`${count} clicks on the button`);
+  $("#count").text(`${count} clicks on the button`);
 };
 
-const debounceUpdateCounter = lodash.debounce(updateCounter, 500);
-$('#btn').on('click', debounceUpdateCounter);
+$("#btn").on("click", lodash.debounce(updateCounter, 500));
