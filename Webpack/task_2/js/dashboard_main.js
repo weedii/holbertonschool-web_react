7064e3ed7c38e783947dfd234231644698c7664d
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-05-04 22:43:10 +0100

    Adding CSS & Images

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -3,7 +3,6 @@ import lodash from 'lodash';
 import "../css/main.css"
 
 const $button = $('<button>Click here to get started</button>');
-$('body').append('<div id="logo"></div>');
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append($button);
