4ec985c8f2bf44c4a21d47522eab4f9fff996eae
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-05-08 23:41:06 +0100

    Dev servers, modules, and tree shaking

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,26 +1,26 @@
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    header: "./modules/header/header.js",
-    body: "./modules/body/body.js",
-    footer: "./modules/footer/footer.js",
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "public/"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public/'),
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   plugins: [new HtmlWebpackPlugin(), new CleanWebpackPlugin()],
   devServer: {
     port: 8564,
   },
   optimization: {
     splitChunks: {
-      chunks: "all",
+      chunks: 'all',
     },
   },
   module: {
@@ -31,12 +31,12 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(jpg|png|svg|gif)$/i,
-        use: ["file-loader", "image-webpack-loader"],
-        type: "asset/resource",
+        use: ['file-loader', 'image-webpack-loader'],
+        type: 'asset/resource',
       },
     ],
   },
