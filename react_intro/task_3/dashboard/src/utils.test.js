558c6328bb565c898584264ae23231e912f0b945
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-05-09 20:55:55 +0100

    Create basic tests with four tests

---
+++ react_intro/task_3/dashboard/src/utils.test.js
@@ -0,0 +1,23 @@
+import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
+
+test("test getFullYear", () => {
+  const currentYear = new Date().getFullYear();
+  expect(getFullYear()).toEqual(currentYear);
+});
+
+test("test getFooterCopy", () => {
+  const result = getFooterCopy(true);
+  expect(result).toEqual("Holberton School");
+});
+
+test("test getFooterCopy", () => {
+  const result = getFooterCopy(false);
+  expect(result).toEqual("Holberton School main dashboard");
+});
+
+test("test getLatestNotification", () => {
+  const result = getLatestNotification();
+  expect(result).toEqual({
+    __html: "<strong>Urgent requirement</strong> - complete by EOD",
+  });
+});
