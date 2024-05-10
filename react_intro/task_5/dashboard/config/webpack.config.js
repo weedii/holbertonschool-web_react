65602564c948abb782a1c7737b8e39f0fce53693
weedii <weedi@DESKTOP-2SCQG2R.localdomain>
2024-05-10 13:18:33 +0100

    Create a project using Webpack

---
+++ react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|png|svg|gif)$/i,
+        use: ["file-loader", "image-webpack-loader"],
+        type: "asset/resource",
+      },
+    ],
+  },
+};
