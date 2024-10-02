c03c8ab954a0ed2794c1e07cb28ef9891ded822a
weedii <abidiw293@gmail.com>
2024-10-02 22:31:41 +0100

    Inline styling

--- React_inline_styling/task_0/dashboard/src/CourseList/CourseList.test.js
+++ React_inline_styling/task_0/dashboard/src/CourseList/CourseList.test.js
@@ -1,54 +1,16 @@
+import { shallow } from "enzyme";
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import CourseListRow from "./CourseListRow";
+import CourseList from "./CourseList";
 
-describe("CourseListRow component tests", () => {
-  test("renders the correct background color when isHeader is true", () => {
-    render(<CourseListRow isHeader={true} textFirstCell="First cell" />);
-
-    const row = screen.getByRole("row");
-    const cell = screen.getByRole("columnheader");
-
-    // Check the row's background color for header
-    expect(row).toHaveStyle({ backgroundColor: "#deb5b545" });
-    expect(cell).toHaveStyle({ backgroundColor: "#deb5b545" });
+// shallow render CourseList component
+describe("<CourseList />", () => {
+  it(`Renders CourseList component without crashing`, () => {
+    const wrapper = shallow(<CourseList />);
+    expect(wrapper.exists()).toBe(true);
   });
 
-  test("renders the correct background color when isHeader is true and secondTextCell is not null", () => {
-    render(
-      <CourseListRow
-        isHeader={true}
-        textFirstCell="First cell"
-        textSecondCell="Second cell"
-      />
-    );
-
-    const row = screen.getByRole("row");
-    const firstCell = screen.getAllByRole("columnheader")[0];
-    const secondCell = screen.getAllByRole("columnheader")[1];
-
-    // Check the row's background color for header with two cells
-    expect(row).toHaveStyle({ backgroundColor: "#deb5b545" });
-    expect(firstCell).toHaveStyle({ backgroundColor: "#deb5b545" });
-    expect(secondCell).toHaveStyle({ backgroundColor: "#deb5b545" });
-  });
-
-  test("renders the correct background color when isHeader is false", () => {
-    render(
-      <CourseListRow
-        isHeader={false}
-        textFirstCell="First cell"
-        textSecondCell="Second cell"
-      />
-    );
-
-    const row = screen.getByRole("row");
-    const firstCell = screen.getAllByRole("cell")[0];
-    const secondCell = screen.getAllByRole("cell")[1];
-
-    // Check the row's background color for non-header row
-    expect(row).toHaveStyle({ backgroundColor: "#f5f5f5ab" });
-    expect(firstCell).toHaveStyle({ backgroundColor: "#f5f5f5ab" });
-    expect(secondCell).toHaveStyle({ backgroundColor: "#f5f5f5ab" });
+  it(`Renders several CourseListRow Components`, () => {
+    const wrapper = shallow(<CourseList />);
+    expect(wrapper.find("CourseListRow").length).toBe(2);
   });
 });
--- React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
+++ React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -1,24 +1,54 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import CourseListRow from './CourseListRow';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import CourseListRow from "./CourseListRow";
 
+describe("CourseListRow component tests", () => {
+  test("renders the correct background color when isHeader is true", () => {
+    render(<CourseListRow isHeader={true} textFirstCell="First cell" />);
 
-// shallow render CourseListRow component
-describe('<CourseListRow />', () => {
-	it(`When isHeader is true, renders on cell with colspan=2
-	when textSecondCell does not exist`, () => {
-		const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" />);
-		expect(wrapper.find('th').exists()).toBe(true);
-	})
+    const row = screen.getByRole("row");
+    const cell = screen.getByRole("columnheader");
 
-	it(`When isHeader is true, renders two cells when textSecondCell is present`, () => {
-		const wrapper = shallow(<CourseListRow isHeader={true} textFirstCell="test" textSecondCell="test2" />);
-		expect(wrapper.find('th').length).toBe(2);
-	})
+    // Check the row's background color for header
+    expect(row).toHaveStyle({ backgroundColor: "#deb5b545" });
+    expect(cell).toHaveStyle({ backgroundColor: "#deb5b545" });
+  });
 
-	it(`When isHeader is false, renders two td elements within a tr element`, () => {
-		const wrapper = shallow(<CourseListRow isHeader={false} textFirstCell="test" textSecondCell="test2" />);
-		expect(wrapper.find('tr').length).toBe(1);
-		expect(wrapper.find('td').length).toBe(2);
-	})
-})
+  test("renders the correct background color when isHeader is true and secondTextCell is not null", () => {
+    render(
+      <CourseListRow
+        isHeader={true}
+        textFirstCell="First cell"
+        textSecondCell="Second cell"
+      />
+    );
+
+    const row = screen.getByRole("row");
+    const firstCell = screen.getAllByRole("columnheader")[0];
+    const secondCell = screen.getAllByRole("columnheader")[1];
+
+    // Check the row's background color for header with two cells
+    expect(row).toHaveStyle({ backgroundColor: "#deb5b545" });
+    expect(firstCell).toHaveStyle({ backgroundColor: "#deb5b545" });
+    expect(secondCell).toHaveStyle({ backgroundColor: "#deb5b545" });
+  });
+
+  test("renders the correct background color when isHeader is false", () => {
+    render(
+      <CourseListRow
+        isHeader={false}
+        textFirstCell="First cell"
+        textSecondCell="Second cell"
+      />
+    );
+
+    const row = screen.getByRole("row");
+    const firstCell = screen.getAllByRole("cell")[0];
+    const secondCell = screen.getAllByRole("cell")[1];
+
+    // Check the row's background color for non-header row
+    expect(row).toHaveStyle({ backgroundColor: "#f5f5f5ab" });
+    expect(firstCell).toHaveStyle({ backgroundColor: "#f5f5f5ab" });
+    expect(secondCell).toHaveStyle({ backgroundColor: "#f5f5f5ab" });
+  });
+});
