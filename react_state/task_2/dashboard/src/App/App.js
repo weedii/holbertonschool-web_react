60cbd15020b8fe4fc595f694d5cfea3c76792dfc
weedii <abidiw293@gmail.com>
2024-10-05 00:21:28 +0100

    Context

--- react_state/task_2/dashboard/src/App/App.js
+++ react_state/task_2/dashboard/src/App/App.js
@@ -1,86 +1,143 @@
-import { shallow } from "enzyme";
-import React from "react";
-import App from "./App";
-import Login from "../Login/Login";
+import React, { PureComponent } from "react";
 import Notifications from "../Notifications/Notifications";
+import { getLatestNotification } from "../utils/utils";
+import Login from "../Login/Login";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
 import CourseList from "../CourseList/CourseList";
+import propTypes from "prop-types";
+import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
+import { StyleSheet, css } from "aphrodite";
+import AppContext from "./AppContext";
 
-describe("<App />", () => {
-  let wrapper;
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  beforeEach(() => {
-    wrapper = shallow(<App />);
-  });
-
-  it("Tests that App renders without crashing", () => {
-    expect(wrapper.exists()).toBe(true);
-  });
-
-  it("Contains Header component", () => {
-    expect(wrapper.find("Header").length).toBe(1);
-  });
-
-  it("Contains Footer component", () => {
-    expect(wrapper.find("Footer").length).toBe(1);
-  });
-
-  it("Tests that CourseList is not displayed when the user is not logged in", () => {
-    expect(wrapper.find(CourseList).length).toBe(0);
-  });
-
-  it("Tests that Login component is displayed when the user is not logged in", () => {
-    expect(wrapper.find(Login).length).toBe(1);
-  });
-
-  it("Tests that CourseList is displayed when the user is logged in", () => {
-    wrapper.setState({
+    this.state = {
+      displayDrawer: false,
       user: {
+        email: "",
+        password: "",
+        isLoggedIn: false,
+      },
+    };
+
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.logOut = this.logOut.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDownLogout);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDownLogout);
+  }
+
+  handleKeyDownLogout = (e) => {
+    if (e.ctrlKey && e.key === "h") {
+      alert("Logging you out");
+      this.logOut();
+    }
+  };
+
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
+
+  logIn(email, password) {
+    this.setState({
+      user: {
+        email: email,
+        password: password,
         isLoggedIn: true,
-        email: "test@example.com",
-        password: "password",
       },
     });
-    expect(wrapper.find(CourseList).length).toBe(1);
-  });
+  }
 
-  it("Tests that Login component is not displayed when the user is logged in", () => {
-    wrapper.setState({
+  logOut() {
+    this.setState({
       user: {
-        isLoggedIn: true,
-        email: "test@example.com",
-        password: "password",
+        email: "",
+        password: "",
+        isLoggedIn: false,
       },
     });
-    expect(wrapper.find(Login).length).toBe(0);
-  });
+  }
 
-  it("Tests that logOut method updates the state and UI", () => {
-    wrapper.setState({
-      user: {
-        isLoggedIn: true,
-        email: "test@example.com",
-        password: "password",
-      },
-    });
-    wrapper.instance().logOut();
-    expect(wrapper.state().user.isLoggedIn).toBe(false);
-    expect(wrapper.find(Login).length).toBe(1);
-    expect(wrapper.find(CourseList).length).toBe(0);
-  });
+  render() {
+    const { displayDrawer, user } = this.state;
+    const isLoggedIn = user.isLoggedIn;
 
-  it("Tests that handleDisplayDrawer is called when Notifications menu is clicked", () => {
-    const instance = wrapper.instance();
-    jest.spyOn(instance, "handleDisplayDrawer");
-    wrapper.instance().handleDisplayDrawer();
-    expect(instance.handleDisplayDrawer).toHaveBeenCalled();
-    expect(wrapper.state().displayDrawer).toBe(true);
-  });
+    const listCourses = [
+      { id: 1, name: "ES6", credit: "60" },
+      { id: 2, name: "Webpack", credit: "20" },
+      { id: 3, name: "React", credit: "40" },
+    ];
 
-  it("Tests that handleHideDrawer is called when the close button is clicked", () => {
-    const instance = wrapper.instance();
-    jest.spyOn(instance, "handleHideDrawer");
-    wrapper.instance().handleHideDrawer();
-    expect(instance.handleHideDrawer).toHaveBeenCalled();
-    expect(wrapper.state().displayDrawer).toBe(false);
-  });
+    const listNotifications = [
+      { id: 1, type: "default", value: "New course available" },
+      { id: 2, type: "urgent", value: "New resume available" },
+      { id: 3, html: { __html: getLatestNotification() }, type: "urgent" },
+    ];
+
+    return (
+      <AppContext.Provider value={{ user, logOut: this.logOut }}>
+        <div className="App">
+          <Notifications
+            listNotifications={listNotifications}
+            displayDrawer={displayDrawer}
+            handleDisplayDrawer={this.handleDisplayDrawer}
+            handleHideDrawer={this.handleHideDrawer}
+          />
+          <Header />
+          <div className={css(styles.body)}>
+            {isLoggedIn ? (
+              <BodySectionWithMarginBottom title="Course List">
+                <CourseList listCourses={listCourses} />
+              </BodySectionWithMarginBottom>
+            ) : (
+              <Login logIn={this.logIn} />
+            )}
+
+            <BodySectionWithMarginBottom title="News from the School">
+              <p>Holberton School News goes here</p>
+            </BodySectionWithMarginBottom>
+          </div>
+          <div className={css(styles.footer)}>
+            <Footer />
+          </div>
+        </div>
+      </AppContext.Provider>
+    );
+  }
+}
+
+App.defaultProps = {};
+
+App.propTypes = {};
+
+const styles = StyleSheet.create({
+  body: {
+    fontFamily: "sans-serif",
+    marginTop: "50px",
+    marginLeft: "30px",
+  },
+  footer: {
+    fontFamily: "sans-serif",
+    fontStyle: "italic",
+    textAlign: "center",
+    position: "absolute",
+    bottom: "0",
+    width: "100%",
+  },
 });
+
+export default App;
--- react_state/task_2/dashboard/src/App/App.test.js
+++ react_state/task_2/dashboard/src/App/App.test.js
@@ -3,6 +3,7 @@ import React from "react";
 import App from "./App";
 import Login from "../Login/Login";
 import Notifications from "../Notifications/Notifications";
+import CourseList from "../CourseList/CourseList";
 
 describe("<App />", () => {
   let wrapper;
@@ -19,41 +20,67 @@ describe("<App />", () => {
     expect(wrapper.find("Header").length).toBe(1);
   });
 
-  it("Contains Login component", () => {
-    expect(wrapper.contains(<Login />)).toBe(true);
-  });
-
   it("Contains Footer component", () => {
     expect(wrapper.find("Footer").length).toBe(1);
   });
 
-  it("Tests that CourseList is not displayed when not logged in", () => {
-    expect(wrapper.find("CourseList").length).toBe(0);
+  it("Tests that CourseList is not displayed when the user is not logged in", () => {
+    expect(wrapper.find(CourseList).length).toBe(0);
+  });
+
+  it("Tests that Login component is displayed when the user is not logged in", () => {
+    expect(wrapper.find(Login).length).toBe(1);
+  });
+
+  it("Tests that CourseList is displayed when the user is logged in", () => {
+    wrapper.setState({
+      user: {
+        isLoggedIn: true,
+        email: "test@example.com",
+        password: "password",
+      },
+    });
+    expect(wrapper.find(CourseList).length).toBe(1);
+  });
+
+  it("Tests that Login component is not displayed when the user is logged in", () => {
+    wrapper.setState({
+      user: {
+        isLoggedIn: true,
+        email: "test@example.com",
+        password: "password",
+      },
+    });
+    expect(wrapper.find(Login).length).toBe(0);
+  });
+
+  it("Tests that logOut method updates the state and UI", () => {
+    wrapper.setState({
+      user: {
+        isLoggedIn: true,
+        email: "test@example.com",
+        password: "password",
+      },
+    });
+    wrapper.instance().logOut();
+    expect(wrapper.state().user.isLoggedIn).toBe(false);
+    expect(wrapper.find(Login).length).toBe(1);
+    expect(wrapper.find(CourseList).length).toBe(0);
   });
 
   it("Tests that handleDisplayDrawer is called when Notifications menu is clicked", () => {
     const instance = wrapper.instance();
-    jest.spyOn(instance, "handleDisplayDrawer"); // Spy on the method
-    wrapper.instance().handleDisplayDrawer(); // Directly call the method
+    jest.spyOn(instance, "handleDisplayDrawer");
+    wrapper.instance().handleDisplayDrawer();
     expect(instance.handleDisplayDrawer).toHaveBeenCalled();
+    expect(wrapper.state().displayDrawer).toBe(true);
   });
 
   it("Tests that handleHideDrawer is called when the close button is clicked", () => {
     const instance = wrapper.instance();
-    jest.spyOn(instance, "handleHideDrawer"); // Spy on the method
-    wrapper.instance().handleHideDrawer(); // Directly call the method
+    jest.spyOn(instance, "handleHideDrawer");
+    wrapper.instance().handleHideDrawer();
     expect(instance.handleHideDrawer).toHaveBeenCalled();
-  });
-});
-
-describe("<App /> when logged in", () => {
-  it("Tests that the Login component is not rendered", () => {
-    const wrapper = shallow(<App isLoggedIn={true} />);
-    expect(wrapper.contains(<Login />)).toBe(false);
-  });
-
-  it("Tests that CourseList component is rendered when logged in", () => {
-    const wrapper = shallow(<App isLoggedIn={true} />);
-    expect(wrapper.find("CourseList").length).toBe(1);
+    expect(wrapper.state().displayDrawer).toBe(false);
   });
 });
